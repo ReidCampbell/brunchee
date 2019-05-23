119f49b6a5d1e01ad2f9918a35e0fff424dfe7e9
Reid Campbell <reidscottcampbell@gmail.com>
2019-05-23 16:15:32 +0100

    fix heroku

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,8 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
-  def home
-    @venue = Venue.new
-    @venues = Venue.first(3)
-  end
+  # def home
+  #   @venue = Venue.new
+  #   @venues = Venue.first(3)
+  # end
 end
