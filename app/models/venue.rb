ffc949f1016565792b331f7dff121ac6f56dca7c
Reid Campbell <reidscottcampbell@gmail.com>
2019-05-24 12:32:31 +0100

    updated models

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,2 +1,13 @@
 class Venue < ApplicationRecord
+  has_many :bookings, dependent: :destroy
+  has_many :reviews, through: :bookings
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
+  mount_uploader :photo, PhotoUploader
+
+  include AlgoliaSearch
+
+  algoliasearch do
+    attribute :address, :post_code, :neighborhood
+  end
 end
