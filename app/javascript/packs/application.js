e22424e48b59f45f0af61bf86949520f31299193
Reid Campbell <reidscottcampbell@gmail.com>
2019-05-29 18:00:55 +0100

    added JS

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,14 +1,7 @@
 import "bootstrap";
-import 'mapbox-gl/dist/mapbox-gl.css';
-
-var client = algoliasearch(ApplicationID, Search-Only-API-Key);
-var index = client.initIndex('YourIndexName');
-index.search('something', { hitsPerPage: 10, page: 0 })
-  .then(function searchDone(content) {
-    console.log(content)
-  })
-  .catch(function searchFailure(err) {
-    console.error(err);
-  });
+import { showHideNavbar } from '../components/navbar';
 
 
+document.addEventListener("DOMContentLoaded", () => {
+  showHideNavbar();
+});
