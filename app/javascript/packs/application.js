59fecf6f54cd7b2b8a7113873514ce4940a91a30
Leonidas <Leonidas@King-Leonidas.local>
2019-05-29 12:42:55 +0100

    mapUpdate

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,14 +1,20 @@
 import "bootstrap";
 import 'mapbox-gl/dist/mapbox-gl.css';
+import { initMapbox } from '../plugins/init_mapbox';
+import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 
-var client = algoliasearch(ApplicationID, Search-Only-API-Key);
-var index = client.initIndex('YourIndexName');
-index.search('something', { hitsPerPage: 10, page: 0 })
-  .then(function searchDone(content) {
-    console.log(content)
-  })
-  .catch(function searchFailure(err) {
-    console.error(err);
-  });
+
+
+// var client = algoliasearch(ApplicationID, Search-Only-API-Key);
+// var index = client.initIndex('YourIndexName');
+// index.search('something', { hitsPerPage: 10, page: 0 })
+//   .then(function searchDone(content) {
+//     console.log(content)
+//   })
+//   .catch(function searchFailure(err) {
+//     console.error(err);
+//   });
+
+  initMapbox();
 
 
