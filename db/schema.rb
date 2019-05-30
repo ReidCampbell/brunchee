1290a3166a2a30c91c26f03adb37f5c278d8c56e
Catherine Nguyen Cat <catanhtho@gmail.com>
2019-05-30 13:58:48 +0100

    show-page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_23_135718) do
+ActiveRecord::Schema.define(version: 2019_05_30_113044) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2019_05_23_135718) do
     t.float "value"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "party_size"
   end
 
   create_table "reviews", force: :cascade do |t|
