e0d875e0a43d3fd6a79957451e9232a2d9e86e8f
Catherine Nguyen Cat <catanhtho@gmail.com>
2019-05-31 13:40:06 +0100

    fixed index page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,3 +5,43 @@ import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the styleshe
 import { initMapbox } from '../plugins/init_mapbox';
 
 initMapbox();
+
+  var sheet = document.createElement('style'),
+  $rangeInput = $('.range input'),
+  prefs = ['webkit-slider-runnable-track', 'moz-range-track', 'ms-track'];
+
+document.body.appendChild(sheet);
+
+var getTrackStyle = function (el) {
+  var curVal = el.value,
+      val = (curVal - 1) * 33.3333,
+      style = '';
+
+  // Set active label
+  $('.range-labels li').removeClass('active selected');
+
+  var curLabel = $('.range-labels').find('li:nth-child(' + curVal + ')');
+
+  curLabel.addClass('active selected');
+  curLabel.prevAll().addClass('selected');
+
+  // Change background gradient
+  for (var i = 0; i < prefs.length; i++) {
+    style += '.range {background: linear-gradient(to right, #37adbf 0%, #37adbf ' + val + '%, #fff ' + val + '%, #fff 100%)}';
+    style += '.range input::-' + prefs[i] + '{background: linear-gradient(to right, #37adbf 0%, #37adbf ' + val + '%, #b2b2b2 ' + val + '%, #b2b2b2 100%)}';
+  }
+
+  return style;
+}
+
+$rangeInput.on('input', function () {
+  sheet.textContent = getTrackStyle(this);
+});
+
+// Change input value on label click
+$('.range-labels li').on('click', function () {
+  var index = $(this).index();
+
+  $rangeInput.val(index + 1).trigger('input');
+
+});
