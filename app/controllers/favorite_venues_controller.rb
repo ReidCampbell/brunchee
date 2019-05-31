d494d889b2b27000987de9d93e7feacc60d682bf
Reid Campbell <reidscottcampbell@gmail.com>
2019-05-31 16:45:00 +0100

    fixed heart

--- app/controllers/favorite_venues_controller.rb
+++ app/controllers/favorite_venues_controller.rb
@@ -12,6 +12,7 @@ class FavoriteVenuesController < ApplicationController
   def destroy
     Favorite.find(params[:id]).destroy
     redirect_back fallback_location: root_path, notice: "#{@venue.name} is no longer in favorites"
+  end
 
   private
 
